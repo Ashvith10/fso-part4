9bfc5075fcda8804d8c83546ed4b6ce82b72a2de
Ashvith Shetty <113123021+Ashvith10@users.noreply.github.com>
2023-05-29 12:19:35 +0530

    Complete step 5 of helper functions and unit tests

--- blog-list/tests/list_helper.test.js
+++ blog-list/tests/list_helper.test.js
@@ -13,6 +13,34 @@ const listWithOneBlog = [
     }
 ]
 
+const listWithUniqueBlogs = [
+    {
+        _id: '5a422a851b54a676234d17f7',
+        title: 'React patterns',
+        author: 'Michael Chan',
+        url: 'https://reactpatterns.com/',
+        likes: 7,
+        __v: 0
+    },
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+    },
+    {
+        _id: '5a422ba71b54a676234d17fb',
+        title: 'TDD harms architecture',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+        likes: 0,
+        __v: 0
+    }
+]
+
+
 const listWithManyBlogs = [
     {
         _id: '5a422a851b54a676234d17f7',
@@ -127,11 +155,51 @@ describe('most blogs', () => {
         })
     })
 
+    test('of a unique list returns 1', () => {
+        const result = listHelper.mostBlogs(listWithUniqueBlogs)
+        expect(result).toEqual({
+            author: 'Michael Chan',
+            blogs: 1
+        })
+    })
+
     test('of a bigger list returns the max count', () => {
         const result = listHelper.mostBlogs(listWithManyBlogs)
         expect(result).toEqual({
             author: 'Robert C. Martin',
             blogs: 3
+        })
+    })
+})
+
+describe('most likes', () => {
+    test('when list is empty, return an empty object', () => {
+        const result = listHelper.mostLikes(listWithNoBlog)
+        expect(result).toEqual({})
+    })
+
+    test('when list has only one blog, return the like', () => {
+        const result = listHelper.mostLikes(listWithOneBlog)
+        expect(result).toEqual({
+            author: 'Edsger W. Dijkstra',
+            likes: 5
+        })
+    })
+
+    test('of a unique list returns the total count of likes', () => {
+        const result = listHelper.mostLikes(listWithUniqueBlogs)
+        expect(result).toEqual({
+            author: 'Michael Chan',
+            likes: 7
+        })
+    })
+
+
+    test('of a bigger list returns the total count of likes', () => {
+        const result = listHelper.mostLikes(listWithManyBlogs)
+        expect(result).toEqual({
+            author: 'Edsger W. Dijkstra',
+            likes: 17
         })
     })
 })
--- blog-list/utils/list_helper.js
+++ blog-list/utils/list_helper.js
@@ -24,11 +24,28 @@ const mostBlogs = (blogs) => {
         }, {})
         const authorWithMaxCount = Object.keys(authorWithBlogCounts)
             .reduce((prevKey, currKey) =>
-                (authorWithBlogCounts[prevKey] > authorWithBlogCounts[currKey])
+                (authorWithBlogCounts[prevKey] >= authorWithBlogCounts[currKey])
                     ? prevKey
                     : currKey)
         return { author: authorWithMaxCount, blogs: authorWithBlogCounts[authorWithMaxCount] }
     }
 }
 
-export default { dummy, totalLikes, favoriteBlog, mostBlogs }
+const mostLikes = (blogs) => {
+    if (blogs.length === 0) {
+        return {}
+    } else {
+        const authorWithLikeCounts = blogs.reduce((orderedBlog, currentBlog) => {
+            orderedBlog[currentBlog.author] = orderedBlog[currentBlog.author] + currentBlog.likes || currentBlog.likes
+            return orderedBlog
+        }, {})
+        const authorWithMaxLikes = Object.keys(authorWithLikeCounts)
+            .reduce((prevKey, currKey) =>
+                (authorWithLikeCounts[prevKey] >= authorWithLikeCounts[currKey])
+                    ? prevKey
+                    : currKey)
+        return { author:authorWithMaxLikes, likes: authorWithLikeCounts[authorWithMaxLikes] }
+    }
+}
+
+export default { dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes }
