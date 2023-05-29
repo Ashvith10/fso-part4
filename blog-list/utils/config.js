59903d95f44ef56fd6ef3db9cf8e70330766d53f
Ashvith Shetty <113123021+Ashvith10@users.noreply.github.com>
2023-05-30 00:54:43 +0530

    Complete step 1 of Blog list tests

--- blog-list/utils/config.js
+++ blog-list/utils/config.js
@@ -2,7 +2,11 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = (process.env.NODE_ENV === 'production')
+    ? process.env.PROD_MONGODB_URI
+    : (process.env.NODE_ENV === 'test')
+        ? process.env.TEST_MONGODB_URI
+        : process.env.DEV_MONGODB_URI
 const PORT = process.env.PORT
 
 export default { MONGODB_URI, PORT }
