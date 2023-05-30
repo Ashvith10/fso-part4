07e065f58029206b6521d24cc30afe8723b89385
Ashvith Shetty <113123021+Ashvith10@users.noreply.github.com>
2023-05-30 21:12:55 +0530

    Complete step 4 of Blog list tests

--- blog-list/tests/blog_api.test.js
+++ blog-list/tests/blog_api.test.js
@@ -45,6 +45,12 @@ const newBlog = {
     likes: 10,
 }
 
+const newBlogWithoutLike = {
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html',
+}
+
 beforeEach(async () => {
     await Blog.deleteMany({})
     await Blog.insertMany(initialBlogs)
@@ -60,9 +66,7 @@ test('blogs are returned as json', async () => {
 test('id exists in blogs', async () => {
     const response = await api.get('/api/blogs')
 
-    response.body.map((blog) =>
-        expect(blog.id).toBeDefined()
-    )
+    response.body.map((blog) => expect(blog.id).toBeDefined())
 })
 
 test('post request creates a new blog', async () => {
@@ -71,11 +75,22 @@ test('post request creates a new blog', async () => {
         .send(newBlog)
         .expect(201)
         .expect('Content-Type', /application\/json/)
+
     const response = await api.get('/api/blogs')
+
     expect(response.body).toHaveLength(initialBlogs.length + 1)
     expect(response.body).toContainEqual(request.body)
 })
 
+test('if "likes" property is missing, default to zero', async () => {
+    const response = await api
+        .post('/api/blogs')
+        .send(newBlogWithoutLike)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    expect(response.body.likes).toBe(0)
+})
 
 afterAll(async () => {
     await mongoose.connection.close()
