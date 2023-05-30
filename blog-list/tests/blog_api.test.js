006f4dbf78c8f3d381fc100af3b91af2438e187c
Ashvith Shetty <113123021+Ashvith10@users.noreply.github.com>
2023-05-30 20:12:33 +0530

    Complete step 3 of Blog list tests

--- blog-list/tests/blog_api.test.js
+++ blog-list/tests/blog_api.test.js
@@ -1,14 +1,53 @@
 import mongoose from 'mongoose'
 import supertest from 'supertest'
-import listHelper from '../utils/list_helper.js'
 import app from '../app.js'
 import Blog from '../models/blog.js'
 
 const api = supertest(app)
 
+const initialBlogs = [
+    {
+        title: 'React patterns',
+        author: 'Michael Chan',
+        url: 'https://reactpatterns.com/',
+        likes: 7,
+    },
+    {
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+    },
+    {
+        title: 'Canonical string reduction',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+        likes: 12,
+    },
+    {
+        title: 'TDD harms architecture',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+        likes: 0,
+    },
+    {
+        title: 'Type wars',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+        likes: 2,
+    }
+]
+
+const newBlog = {
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html',
+    likes: 10,
+}
+
 beforeEach(async () => {
     await Blog.deleteMany({})
-    await Blog.insertMany(listHelper.listWithManyBlogs)
+    await Blog.insertMany(initialBlogs)
 })
 
 test('blogs are returned as json', async () => {
@@ -25,6 +64,18 @@ test('id exists in blogs', async () => {
         expect(blog.id).toBeDefined()
     )
 })
+
+test('post request creates a new blog', async () => {
+    const request = await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(initialBlogs.length + 1)
+    expect(response.body).toContainEqual(request.body)
+})
+
 
 afterAll(async () => {
     await mongoose.connection.close()
