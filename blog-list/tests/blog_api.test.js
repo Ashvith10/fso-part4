e2278e01abd17288853d61165d2562203e7a8789
Ashvith Shetty <113123021+Ashvith10@users.noreply.github.com>
2023-06-04 00:18:41 +0530

    chore: Refactor tests with helper

--- blog-list/tests/blog_api.test.js
+++ blog-list/tests/blog_api.test.js
@@ -1,53 +1,14 @@
 import mongoose from 'mongoose'
 import supertest from 'supertest'
 import app from '../app.js'
+import helper from './test_helper.js'
 import Blog from '../models/blog.js'
 
 const api = supertest(app)
 
-const initialBlogs = [
-    {
-        title: 'React patterns',
-        author: 'Michael Chan',
-        url: 'https://reactpatterns.com/',
-        likes: 7,
-    },
-    {
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-        likes: 5,
-    },
-    {
-        title: 'Canonical string reduction',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-        likes: 12,
-    },
-    {
-        title: 'TDD harms architecture',
-        author: 'Robert C. Martin',
-        url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-        likes: 0,
-    },
-    {
-        title: 'Type wars',
-        author: 'Robert C. Martin',
-        url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-        likes: 2,
-    }
-]
-
-const newBlog = {
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html',
-    likes: 10,
-}
-
 beforeEach(async () => {
     await Blog.deleteMany({})
-    await Blog.insertMany(initialBlogs)
+    await Blog.insertMany(helper.initialBlogs)
 })
 
 
@@ -62,7 +23,7 @@ describe('when there is initially some blogs saved', () => {
     test('all notes are returned', async () => {
         const response = await api.get('/api/blogs')
 
-        expect(response.body).toHaveLength(initialBlogs.length)
+        expect(response.body).toHaveLength(helper.initialBlogs.length)
     })
 
     test('id exists in blogs', async () => {
@@ -75,18 +36,18 @@ describe('when there is initially some blogs saved', () => {
         test('succeeds with valid data', async () => {
             const postResponse = await api
                 .post('/api/blogs')
-                .send(newBlog)
+                .send(helper.newBlog)
                 .expect(201)
                 .expect('Content-Type', /application\/json/)
 
-            const getResponse = await api.get('/api/blogs')
+            const blogsAtEnd = await helper.blogsInDb()
 
-            expect(getResponse.body).toHaveLength(initialBlogs.length + 1)
-            expect(getResponse.body).toContainEqual(postResponse.body)
+            expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+            expect(blogsAtEnd).toContainEqual(postResponse.body)
         })
 
         test('default to zero if "likes" property is missing', async () => {
-            const { likes, ...newBlogWithoutLike } = newBlog
+            const { likes, ...newBlogWithoutLike } = helper.newBlog
             const response = await api
                 .post('/api/blogs')
                 .send(newBlogWithoutLike)
@@ -98,7 +59,7 @@ describe('when there is initially some blogs saved', () => {
 
         describe('responds with code 400', () => {
             test('if "title" is missing', async () => {
-                const { title, ...newBlogWithoutTitle } = newBlog
+                const { title, ...newBlogWithoutTitle } = helper.newBlog
 
                 await api
                     .post('/api/blogs')
@@ -108,7 +69,7 @@ describe('when there is initially some blogs saved', () => {
 
 
             test('if "url" is missing', async () => {
-                const { url, ...newBlogWithoutUrl } = newBlog
+                const { url, ...newBlogWithoutUrl } = helper.newBlog
 
                 await api
                     .post('/api/blogs')
@@ -117,7 +78,7 @@ describe('when there is initially some blogs saved', () => {
             })
 
             test('if both "title" and "url" are missing', async () => {
-                const { url, title, ...newBlogWithoutTitleOrUrl } = newBlog
+                const { url, title, ...newBlogWithoutTitleOrUrl } = helper.newBlog
 
                 await api
                     .post('/api/blogs')
@@ -129,46 +90,40 @@ describe('when there is initially some blogs saved', () => {
 
     describe('deletion of a note', () => {
         test('suceeds with status code 204, if "id" is valid', async () => {
-            const getResponseBeforeDelete = await api
-                .get('/api/blogs')
+            const blogsAtStart = await helper.blogsInDb()
 
             const randomIndex = Math.floor(Math.random()
-                * getResponseBeforeDelete.body.length)
-            const randomBlog = getResponseBeforeDelete.body[randomIndex]
+                * blogsAtStart.length)
+            const randomBlog = blogsAtStart[randomIndex]
 
             await api
                 .delete(`/api/blogs/${randomBlog.id}`)
                 .expect(204)
 
-            const getResponseAfterDelete = await api
-                .get('/api/blogs')
+            const blogsAtEnd = await helper.blogsInDb()
 
-            expect(getResponseAfterDelete.body)
-                .toHaveLength(getResponseBeforeDelete.body.length - 1)
-            expect(getResponseAfterDelete.body).not.toContainEqual(randomBlog)
+            expect(blogsAtEnd).toHaveLength(blogsAtStart.length - 1)
+            expect(blogsAtEnd).not.toContainEqual(randomBlog)
         })
     })
 
     describe('updation of a note', () => {
         test('succeeds with a status code 200', async () => {
-            const getResponseBeforeUpdate = await api
-                .get('/api/blogs')
+            const blogsAtStart = await helper.blogsInDb()
 
             const randomIndex = Math.floor(Math.random()
-                * getResponseBeforeUpdate.body.length)
-            const randomBlog = getResponseBeforeUpdate.body[randomIndex]
+                * blogsAtStart.length)
+            const randomBlog = blogsAtStart[randomIndex]
 
             await api
                 .put(`/api/blogs/${randomBlog.id}`)
-                .send({ ...newBlog })
+                .send({ ...helper.newBlog })
                 .expect(200)
 
-            const getResponseAfterUpdate = await api
-                .get('/api/blogs')
+            const blogsAtEnd = await helper.blogsInDb()
 
-            expect(getResponseAfterUpdate.body)
-                .toHaveLength(getResponseBeforeUpdate.body.length)
-            expect(getResponseAfterUpdate.body).not.toContainEqual(randomBlog)
+            expect(blogsAtEnd).toHaveLength(blogsAtEnd.length)
+            expect(blogsAtEnd).not.toContainEqual(randomBlog)
         })
     })
 })
---
+++ blog-list/tests/test_helper.js
@@ -0,0 +1,30 @@
+import Blog from '../models/blog.js'
+
+const initialBlogs = [
+    {
+        title: 'React patterns',
+        author: 'Michael Chan',
+        url: 'https://reactpatterns.com/',
+        likes: 7,
+    },
+    {
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+    }
+]
+
+const newBlog = {
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html',
+    likes: 10,
+}
+
+const blogsInDb = async () => {
+    const blogs = await Blog.find({})
+    return  blogs.map(blog => blog.toJSON())
+}
+
+export default { initialBlogs, newBlog, blogsInDb }
