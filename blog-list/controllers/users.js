c9e8cec77368dd71d6088e245390fe1e4ae1d81b
Ashvith Shetty <113123021+Ashvith10@users.noreply.github.com>
2023-06-06 14:05:02 +0530

    Complete step 3 of Blog list expansions

---
+++ blog-list/controllers/users.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import bcrypt from 'bcrypt'
+import User from '../models/user.js'
+
+const userRouter = express.Router()
+
+userRouter.post('/', async (request, response) => {
+    const { username, name, password } = request.body
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+
+    const user = new User({ username, name, passwordHash })
+    const savedUser = await user.save()
+
+    response.status(201).json(savedUser)
+})
+
+export default userRouter
---
+++ blog-list/tests/user_api.test.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+import supertest from 'supertest'
+import app from '../app.js'
+import helper from './test_helper.js'
+import User from '../models/user.js'
+
+const api = supertest(app)
+
+describe('when there is initially one user in db', () => {
+    beforeEach(async () => {
+        await User.deleteMany({})
+        const passwordHash = await bcrypt.hash('password', 10)
+        const user = new User({ username: 'user', passwordHash })
+
+        await user.save()
+    })
+
+    test('creation succeeds with a fresh username', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'ashvith',
+            name: 'Ashvith Shetty',
+            password: 'password'
+        }
+
+        await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+        const usernames = usersAtEnd.map(u => u.username)
+        expect(usernames).toContain(newUser.username)
+    })
+
+    afterAll(async () => {
+        await mongoose.connection.close()
+    })
+})
