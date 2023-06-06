2e336f5976678df535145283b52dc8023fd760fb
Ashvith Shetty <113123021+Ashvith10@users.noreply.github.com>
2023-06-06 23:24:47 +0530

    Complete step 4 of Blog list expansions

--- blog-list/controllers/users.js
+++ blog-list/controllers/users.js
@@ -4,16 +4,31 @@ import User from '../models/user.js'
 
 const userRouter = express.Router()
 
-userRouter.post('/', async (request, response) => {
+userRouter.post('/', async (request, response, next) => {
     const { username, name, password } = request.body
+    let error = ''
+
+    if (!password) {
+        error = '"password" is missing'
+    } else if (password.length < 3) {
+        error = '"password" must be at least 3 characters long'
+    }
+
+    if (error !== ''){
+        next(new Error(error))
+        return response.status(400).json({ error: error })
+    }
 
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(password, saltRounds)
-
     const user = new User({ username, name, passwordHash })
-    const savedUser = await user.save()
 
-    response.status(201).json(savedUser)
+    try{
+        const savedUser = await user.save()
+        response.status(201).json(savedUser)
+    } catch (exception) {
+        next(exception)
+    }
 })
 
 export default userRouter
--- blog-list/tests/user_api.test.js
+++ blog-list/tests/user_api.test.js
@@ -38,6 +38,116 @@ describe('when there is initially one user in db', () => {
         expect(usernames).toContain(newUser.username)
     })
 
+    describe('user creation', () => {
+        describe('succeeds when', () => {
+            test('username is three characters or more', async () => {
+                const newUser = {
+                    username: 'ash',
+                    name: 'Ashvith Shetty',
+                    password: 'password'
+                }
+
+                const request = await api
+                    .post('/api/users')
+                    .send(newUser)
+                    .expect(201)
+                    .expect('Content-Type', /application\/json/)
+
+                expect(request.body.username).toEqual(newUser.username)
+                expect(request.body.name).toEqual(newUser.name)
+            })
+
+            test('password is three characters or more', async () => {
+                const newUser = {
+                    username: 'ashvith',
+                    name: 'Ashvith Shetty',
+                    password: 'pas'
+                }
+
+                const request = await api
+                    .post('/api/users')
+                    .send(newUser)
+                    .expect(201)
+                    .expect('Content-Type', /application\/json/)
+
+                expect(request.body.username).toEqual(newUser.username)
+                expect(request.body.name).toEqual(newUser.name)
+            })
+        })
+
+        describe('fails when', () => {
+            describe('username is', () => {
+                test('missing', async () => {
+                    const newUser = {
+                        name: 'Ashvith Shetty',
+                        password: 'password'
+                    }
+
+                    const request = await api
+                        .post('/api/users')
+                        .send(newUser)
+                        .expect(400)
+                        .expect('Content-Type', /application\/json/)
+
+                    expect(request.body)
+                        .toEqual({ error: 'User validation failed: username: Path `username` is required.' })
+                })
+
+                test('less than three characters', async () => {
+                    const newUser = {
+                        username: 'as',
+                        name: 'Ashvith Shetty',
+                        password: 'password'
+                    }
+
+                    const request = await api
+                        .post('/api/users')
+                        .send(newUser)
+                        .expect(400)
+                        .expect('Content-Type', /application\/json/)
+
+                    expect(request.body)
+                        .toEqual({ error: 'User validation failed: username: Path `username` (`as`) is shorter than the minimum allowed length (3).' })
+                })
+            })
+
+            describe('password is', () => {
+                test('missing', async () => {
+                    const newUser = {
+                        username: 'ashvith',
+                        name: 'Ashvith Shetty'
+                    }
+
+                    const request = await api
+                        .post('/api/users')
+                        .send(newUser)
+                        .expect(400)
+                        .expect('Content-Type', /application\/json/)
+
+                    expect(request.body)
+                        .toEqual({ 'error': '"password" is missing' })
+                })
+
+                test('less than three characters', async () => {
+                    const newUser = {
+                        username: 'ashvith',
+                        name: 'Ashvith Shetty',
+                        password: 'pa'
+                    }
+
+                    const request = await api
+                        .post('/api/users')
+                        .send(newUser)
+                        .expect(400)
+                        .expect('Content-Type', /application\/json/)
+
+                    expect(request.body)
+                        .toEqual({ error: '"password" must be at least 3 characters long' })
+                })
+            })
+        })
+    })
+
     afterAll(async () => {
         await mongoose.connection.close()
     })
