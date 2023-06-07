640fbef1b5b943a491e8700af2bdb408e0af215a
Ashvith Shetty <113123021+Ashvith10@users.noreply.github.com>
2023-06-07 20:15:38 +0530

    Complete step 6 of Blog list expansions

--- blog-list/app.js
+++ blog-list/app.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose'
 
 import blogsRouter from './controllers/blogs.js'
 import usersRouter from './controllers/users.js'
+import loginRouter from './controllers/login.js'
 import config from './utils/config.js'
 import logger from './utils/logger.js'
 import middleware from './utils/middleware.js'
@@ -24,6 +25,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.errorHandler)
 
 export default app
